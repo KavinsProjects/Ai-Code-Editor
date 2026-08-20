eef1909691ea9de1b65e1da08a711ffb8a537585
unknown <kavinkanagaraj16@gmail.com>
2026-08-20 20:00:38 +0530

    Next.JS config

---
+++ auth.config.ts
@@ -0,0 +1,16 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export default {
+    providers : [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            clientId : process.env.GOOGLE_ID,
+            clientSecret : process.env.GOOGLE_SECRET
+        }),
+    ],
+}satisfies NextAuthConfig
